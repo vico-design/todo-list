bd2842352952fb22c48545261671cb7d0453b4e1
Florencia Petrone <florenciapetrone@MacBook-Air-de-Florencia.local>
2020-06-19 11:58:39 +0200

    comentarios

--- modules/task.js
+++ modules/task.js
@@ -1,4 +1,5 @@
 const { checkHandler } = require("./list");
+// Agregar nuevas tareas 
 
 class Task {
     constructor(text, completed){
@@ -19,7 +20,7 @@ class Task {
         this.label.appendChild(labelText);
         if (this.completed){
             this.label.className = "completed";
-        }
+        };
         
         const trashIcon = document.createTextNode("🗑");
         this.a.className = "trashButton";
@@ -30,25 +31,30 @@ class Task {
         this.element.appendChild(this.input);
         this.element.appendChild(this.label);
         this.element.appendChild(this.a);
-    }
+        };
     onCheck(params) {
         this.label.className = "completed";
         this.input.onclick = this.onUnCheck;
         this.completed = true;
-        
-    }
+    };
     onDelete() {
         console.log("delete");
-    }
+    };
     onUnCheck() {
         this.label.classList.remove("completed");
         this.input.onclick = this.onCheck; 
         this.completed = false;
-    }
-    
-}
+    };
+};
 const task1 = new Task ("Finish ToDo App", true);
-
 const task2 = new Task ("Drink 3 liter of water", false);
 const task3 = new Task ("Learn more German", false);
 const task4 = new Task ("Do excercise", false);
+
+//conocer las tareas que contiene(array de las tareas)/ agregar y borrar tareas. SIN PROGRESSBAR
+// las nuevas instancias de las tareas tienen que estar dentro de la clase List. 
+
+class List{
+    constructor
+
+}
